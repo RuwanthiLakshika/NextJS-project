ce594a5518b9488a320a31f45b2ee98c248b88c5
RuwanthiLakshika <daruwanthilakshika@gmail.com>
2024-08-14 16:38:46 +0530

    change the font size in profile page

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,10 +1,10 @@
 export default function UserProfile({params}:any){
     return(
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile</h1>
+            <h1 className="text-2xl mb-6">Profile</h1>
             <hr/>
-            <p className="text-4xl">Profile page 
-                <span className="p-2 ml-2 rounded bg-orage-500 text-black">{params.id}</span></p>
+            <p className="text-1xl ">Profile page 
+                <span className="p-2 ml-2 rounded bg-orange-500 text-black ">{params.id}</span></p>
         </div>
     )
 }
